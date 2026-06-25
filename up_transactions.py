119c33e5012e62a7eccad78ce93e4fa5910d78fb
trainingskumar <108652786+trainingsrepo@users.noreply.github.com>
2026-06-25 20:29:02 +0530

    Create up_transactions.py

---
+++ up_transactions.py
@@ -0,0 +1,6 @@
+def init (self,name,up_id,bank_account):
+  self.name=name
+  self.upi_id=upi_id
+  self.bank_account=bank_account
+transaction=Transaction(user1,user2,5000)
+print(transaction.make_transaction())
