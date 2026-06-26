dabcd9e4b1aa7e68599e0f782f54f690f61966e3
trainingsrepo <trainings.anil@gmail.com>
2026-06-26 20:16:13 +0530

    Application.java file added

---
+++ Application.java
@@ -0,0 +1,19 @@
+import java.lang.*; //including Java packages used by this program
+import java.sql.*;
+import com.mishra.*;
+
+public class Account
+{
+	private String Username, Password, Password1, Name;
+
+	public Account(String UN, String PassW, String PassW1, String NM) {
+		Username = UN;
+		Password = PassW;
+		Password1 = PassW1;
+		Name = NM;
+	}
+
+	public Account(String UN, String PassW) {
+		Username = UN;
+		Password = PassW;
+	}
